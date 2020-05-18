2069f5e0503b3a4700271fa6df13a0742d9d4e40
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2020-05-18 21:41:04 +1200

    Added Animal

---
+++ Animal.java
@@ -0,0 +1,33 @@
+
+public class Animal {
+	private String animalType;
+	private int purchacePrice;
+	private int happiness;
+	private int baseReward;
+	private int health;
+	
+	public Animal() {
+		
+	}
+	
+	public String getAnimalType() {
+		return animalType;
+	}
+	
+	public int getPurchacePrice() {
+		return purchacePrice;
+	}
+	
+	public int getHappiness() {
+		return happiness;
+	}
+	
+	public int getBaseReward() {
+		return baseReward;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+
+}

cbb715cacaced6a3f94be7596aeb2f68b980e518
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2020-05-18 23:44:53 +1200

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -14,6 +14,8 @@ public class Animal {
 		health = healthLevel;
 	}
 	
+	// Getter Methods
+	
 	public String getAnimalType() {
 		return animalType;
 	}
@@ -34,27 +36,63 @@ public class Animal {
 		return health;
 	}
 	
+	public int getDailyReward() {
+		return (happiness + health) * baseReward;
+	}
+	
+	// Setter Methods
+	
+	public void setHappiness(int value) {
+		if ((happiness + value <= 10) && (happiness + value >= 0)) {
+			happiness = value;
+		} else if (happiness + value >= 0) {
+			happiness = 0;
+		} else {
+			happiness = 10;
+		}
+	}
+	
+	public void setHealth(int value) {
+		if ((health + value <= 10) && (health + value >= 0)) {
+			health = value;
+		} else if (health + value >= 0) {
+			health = 0;
+		} else {
+			health = 10;
+		}
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Animal animal = new Animal();
 		System.out.println(farmer);
 	}
+	
+	
+	public void feedItem(AnimalItem item) {
+		item.getHappiness()
+		item.getHealth()
+	}
+	
 
 }
 
 public class Cow extends Animal {
 	public Cow() {
-		super("cow", 200, 2, 200, 5);
+		super("cow", 200, 2, 20, 5);
 	}
 }
 
 public class Chicken extends Animal {
 	public Chicken() {
-		super("chicken", 50, 5, 100, 5);
+		super("chicken", 50, 5, 10, 5);
 	}
 }
 
 public class Pig extends Animal {
 	public Pig() {
-		super("pig", 150, 2, 200, 2);
+		super("pig", 150, 2, 20, 2);
 	}
 }

11921e23bbd396946e577f732799b4d99db2e56d
dalai16 <williamhuang777@hotmail.com>
2020-05-24 02:35:18 +1200

    javadoc ani and aitem

--- Game/src/Game/AnimalItem.java
+++ Game/src/Game/AnimalItem.java
@@ -6,15 +6,38 @@ package Game;
  */
 public class AnimalItem {
 	/**
-	 * name identifies the 
+	 * name identifies the name of the
+	 * animal item to differentiate item type
+	 * and thus its other attributes
 	 */
 	private String name;
+	/**
+	 * cost denotes the cost to purchase
+	 * the item in the shop
+	 */
 	private int cost;
+	/**
+	 * health indicates how much health
+	 * the item will restore to the
+	 * animal when consumed
+	 */
 	private int health;
+	/**
+	 * happiness indicates how much happiness
+	 * the item will restore to the 
+	 * animal when consumed
+	 */
 	private int happiness;
 	
-	// matches the string to create item with corresponding values
+	/**
+	 * matches the string to create item with corresponding values
+	 * @param type: the string differentiates which item type will be instantiated when constructed
+	 *  
+	 */
 	public AnimalItem(String type) {
+		/**
+		 * sets string name attribute to @param
+		 */
 		name = type;
 		
 		if (type == "hay") {
@@ -32,20 +55,43 @@ public class AnimalItem {
 		}
 	}
 	
-	// getter methods
-	
+
+	/**
+	 * name getter method
+	 * @return String name of animal item
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * cost getter method
+	 * @return int cost of the animal item from shop 
+	 */
 	public int getCost() {
 		return cost;
 	}
+	/**
+	 * happiness getter method 
+	 * @return the happiness restored to
+	 * the animal when item is consumed
+	 */
 	public int getHappiness() {
 		return happiness;
 	}
+	/**
+	 * health getter method
+	 * @return the health restored
+	 * to animal when item is consumed
+	 */
 	public int getHealth() {
 		return health;
 	}
+	/**
+	 * prints the animal item in 
+	 * a readable format to see the attribute 
+	 * restored amounts for the reader
+	 * @return String consisting of item name and its restore values 
+	 */
 	public String toString() {
 		return name + ":  restores " + health + " and " + happiness + " happiness"; 
 	}

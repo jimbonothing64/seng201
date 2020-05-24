12a8694dd0ffb82ddbf5eb72f59aab45c7eb64c6
dalai16 <williamhuang777@hotmail.com>
2020-05-24 22:05:24 +1200

    Update Main.java

--- Game/src/Game/Main.java
+++ Game/src/Game/Main.java
@@ -1,61 +1,88 @@
 package Game;
-/**
- *  the main game engine that runs the whole game
+/* the main game engine that runs the whole game
+/** the main game engine that runs the whole game
  *  by tying all classes together
  * static variables that are used throughout the game are instantiated
  * run this class to play the game.
+ *  static variables that are used throughout the game are instantiated
+ *  run this class to play the game.
  */
 public class Main {
 	/**
-	 * set Farm variable to pass around the object through
-	 * to other windows
+	 * stores player's farm
 	 */
 	private Farm farm;
+	
 	/**
-	 * @param newFarm takes the farm object created 
-	 * in farm setup and stores it
+	 * sets farm to be used for the game
+	 * @param newFarm the new farm object to be used this game
 	 */
 	public void setFarm(Farm newFarm) {
 		farm = newFarm;
 	}
+	
 	/**
-	 * @return the stored farm object 
+	 * used for retrieving farm for this game
+	 * @return farm so that it can be read
 	 */
 	public Farm getFarm() {
 		return farm;
 	}
+	
 	/**
-	 * 
-	 * @param outManager
+	 * starts game by launching setup screen	
+	 * @param outManager the Main object to be passed into FarmSetupWindow
 	 */
 	public void launchSetupWindow(Main outManager) {
 		FarmSetupWindow setupWindow = new FarmSetupWindow(outManager);
 	}
 	
+	/**
+	 * go from setup to main game screen
+	 * @param setupWindow is the previous window which is to be closed
+	 */
 	public void closeSetupWindow(FarmSetupWindow setupWindow) {
 		setupWindow.closeWindow();
 		launchGameWindow();
 	}
 	
+	/**
+	 * launch new instance of GameWindow passing in Main object
+	 */
 	public void launchGameWindow() {
 		GameWindow gameWindow = new GameWindow(this);
 	}
 	
+	/**
+	 * close game window and open marketplace
+	 * @param gameWindow is closed
+	 */
 	public void closeGameWindow(GameWindow gameWindow) {
 		gameWindow.closeWindow();
 		launchMarketWindow();
 		
 	}
 	
+	/**
+	 * open new instance of market window passing in Main object
+	 */
 	public void launchMarketWindow() {
 		MarketWindow marketWindow = new MarketWindow(this);
 	}
 	
+	/**
+	 * go from market window to main window
+	 * @param marketWindow
+	 */
 	public void closeMarketWindow(MarketWindow marketWindow) {
 		marketWindow.closeWindow();
 		launchGameWindow();
 	}
 	
+	/**
+	 * runs a new game
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Main manager = new Main();
 		manager.launchSetupWindow(manager);

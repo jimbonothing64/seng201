6c39499016514a2deb051507a22f4077e2d1c7bd
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2020-05-24 20:46:26 +1200

    yeetus

--- Game/src/Game/Farm.java
+++ Game/src/Game/Farm.java
@@ -92,6 +92,7 @@ public class Farm {
 		totalDays = days;
 		currentDay = 0;
 		happyDecay = 1;
+		//set bonus based on farm type player selected
 		cropItems.add(new CropItem("water"));
 		if (type == "Swiss Alps Meadow") {
 			pettingBonus = 1;
@@ -217,6 +218,7 @@ public class Farm {
 	 */
 	public ArrayList<String> getCropVarieties() {
 		 ArrayList<String> cropVarieties = new ArrayList<String>();
+		 //add only new types of crop to array
 		for (Crop crop : crops) {
 			if (!(cropVarieties.contains(crop.getName()))) {
 				cropVarieties.add(crop.getName());
@@ -252,6 +254,7 @@ public class Farm {
 	 * @param item is removed from inventory
 	 */
 	public void consumeAnimalItem(AnimalItem item) {
+		//remove first instance of animal item
 		for (AnimalItem animalItem : animalItems) {
 			if (animalItem.getName() == item.getName()) {
 				animalItems.remove(animalItem);
@@ -264,7 +267,9 @@ public class Farm {
 	 * @param item is removed from inventory
 	 */
 	public void consumeCropItem(CropItem item) {
+		//remove first instance of crop item
 		for (CropItem cropItem : cropItems) {
+			//if water do not consume
 			if (cropItem.getName() == "water") {
 				break;
 			}
@@ -282,6 +287,7 @@ public class Farm {
 	 */
 	public void useCropItem(Crop inCrop, CropItem inCropItem) {
 		actionPoints += 1;
+		//use item on all of a particular crop type
 		for (Crop crop : crops) {
 			if (crop.getName() == inCrop.getName()) {
 				crop.useItem(inCropItem);
@@ -296,6 +302,7 @@ public class Farm {
 	 */
 	public void feedAnimals(AnimalItem item) {
 		actionPoints += 1;
+		//feed each animal
 		for (int i = 0; i < animals.size(); i++) {
 			animals.get(i).feedItem(item);
 		}
@@ -308,6 +315,7 @@ public class Farm {
 	 */
 	public void petAnimals() {
 		actionPoints += 1;
+		//pet each animal adding a bonus if needed
 		for (int i = 0; i < animals.size(); i++) {
 			animals.get(i).pet(pettingBonus);
 		}
@@ -339,6 +347,7 @@ public class Farm {
 	 */
 	public int harvest() {
 		int total = 0;
+		//harvest all harvestable crops
 		for (Crop crop: crops) {
 			if(crop.getHarvestable() == 0) {
 				total += crop.getIncome();
@@ -346,12 +355,14 @@ public class Farm {
 		}
 		if (total == 0) return 0;
 		actionPoints += 1;
+		//get new array less harvested crops
 		ArrayList<Crop> updatedArray = new ArrayList<Crop>();
 		for (Crop crop: crops) {
 			if (crop.getHarvestable() != 0) {
 				updatedArray.add(crop);
 			}
 		}
+		//add profit to money and update crops list
 		money += total;
 		crops = updatedArray;
 		return total;
@@ -384,10 +395,10 @@ public class Farm {
 			animal.setHealth(animal.getHealth() - 1);
 		}
 		
-		
+		//add earnings
 		money += earnings;
 		currentDay += 1;
-		
+		//rest action points
 		actionPoints = 0;
 		return earnings;
 	}
